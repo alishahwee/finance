ad694c7547372a76c5491bc698abd0a06925be0d
alishahwee <alisha.hwee@outlook.com>
2020-10-17 04:31:40 +0000

    Implement user registration

--- application.py
+++ application.py
@@ -6,8 +6,12 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+from dotenv import load_dotenv
 
 from helpers import apology, login_required, lookup, usd
+
+# Load environment variables
+load_dotenv()
 
 # Configure application
 app = Flask(__name__)
@@ -22,6 +26,7 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
 
 # Custom filter
 app.jinja_env.filters["usd"] = usd
@@ -65,9 +70,6 @@ def history():
 def login():
     """Log user in"""
 
-    # Forget any user_id
-    session.clear()
-
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
@@ -80,17 +82,22 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username"))
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = :username",
+            username=request.form.get("username"),
+        )
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
+        flash("You were successfully logged in.")
         return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
@@ -106,6 +113,7 @@ def logout():
     session.clear()
 
     # Redirect user to login form
+    flash("You have been logged out.")
     return redirect("/")
 
 
@@ -119,7 +127,47 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm_pw = request.form.get("confirmation")
+
+        # Ensure username was submitted
+        if not username:
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not password:
+            return apology("must provide password", 403)
+
+        # Ensure password confirmation
+        elif password != confirm_pw:
+            return apology("passwords do not match", 403)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = :username",
+            username=username,
+        )
+
+        # Ensure username has not been taken
+        if len(rows) == 1:
+            return apology("username already exists", 403)
+
+        # Create a new user in the database and redirect to login page
+        else:
+            db.execute(
+                "INSERT INTO users (username, hash) VALUES (:username, :hash)",
+                username=username,
+                hash=generate_password_hash(password)
+            )
+            flash("Account registered. You may now login.")
+            return redirect("/login")
+
+    else:
+        return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
